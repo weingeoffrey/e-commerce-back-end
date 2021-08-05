6d5b9f24d750e27dafa8a4c9e203ba48263e9869
weinge <wein.geoffrey@gmail.com>
2021-08-05 16:08:46 -0500

    product model

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,7 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const Category = require('./Category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -9,7 +10,48 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // id column
+    id: {
+      // int, no null, primary key, auto increment
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // product name column
+    product_name: {
+      // str, no null
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    // price column
+    price: {
+      // dec, no null, validate dec=true
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    // stock column
+    stock: {
+      // int, no null, default value of 10, validate isNum
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    // category id field
+    category_id: {
+      // int, ref category id field
+      type: DataTypes.INTEGER,
+      references: {
+        model: "Category",
+        key: "id"
+      }
+    }
   },
   {
     sequelize,
