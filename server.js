eeda2fd726c1964d655da2351404a579e589e1c3
weinge <wein.geoffrey@gmail.com>
2021-08-05 15:48:14 -0500

    Created sequelize connection and env variable

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+const squelize = require('./config/connection')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
